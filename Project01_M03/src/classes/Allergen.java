3be99d07552628c2e0d11eb029c45667bcb011d9
Jordi Felip <jordifelipzayas@gmail.com>
2015-10-01 23:37:09 +0200

    Allergen class y algunas pruebas en ese arraylist. A ver que os parecen. Hice un metodo que puede ser util en un futuro.Podeis borrar sin miedo XD

--- Project01_M03/src/classes/Allergen.java
+++ Project01_M03/src/classes/Allergen.java
@@ -1,27 +1,39 @@
 package classes;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
+// import java.util.ArrayList;
+// import java.util.List;
 
 public class Allergen {
 	// the code is the index of the arrayList //
-	private List<String> allergens;
+	
+	//private List<String> allergens;
+	private String name;
+	private int code;
+	public Allergen(int code, String name){
+		//allergens = new ArrayList<String>();
+		this.name = name;
+		this.code = code;
+	}
 
-	public Allergen(){
-		allergens = new ArrayList<String>();
-	}
-	// getter //
-	public List<String> getAllergens() {
-		return allergens;
+	public int getCode() {
+		return code;
 	}
 
-	// setter //
-	public void setAllergens(List<String> allergens) {
-		this.allergens = allergens;
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
-	
-	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
 }
--- Project01_M03/src/main/Main.java
+++ Project01_M03/src/main/Main.java
@@ -1,10 +1,59 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// we import all classes added in the package classes //
+import classes.*;
+
 public class Main {
 
+	private static void addElements(List<Allergen> allergens) {
+		// TODO Auto-generated method stub
+		allergens.add(new Allergen(0,"Gluten"));
+		allergens.add(new Allergen(1,"Shellfish"));
+		allergens.add(new Allergen(2,"Eggs"));
+		allergens.add(new Allergen(3,"Fish"));
+		allergens.add(new Allergen(4,"Peanuts"));
+		allergens.add(new Allergen(5,"Soy"));
+		allergens.add(new Allergen(6,"Dairy"));
+		allergens.add(new Allergen(7,"Nuts"));
+		allergens.add(new Allergen(8,"Celery"));
+		allergens.add(new Allergen(9,"Mustard"));
+		allergens.add(new Allergen(10,"Sesame"));
+		allergens.add(new Allergen(11,"Sulphites"));
+		allergens.add(new Allergen(12,"Mollusk"));
+		allergens.add(new Allergen(13,"Lupin"));
+	}
+	
+	private static int lookAllergen(String b, List<Allergen> allergens) {
+		int pos = 0;
+		for (Allergen a : allergens) {
+			if(a.getName() == b){
+				pos =  a.getCode();
+			}
+		}
+		return pos;
+	}
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// we create one arraylist of allergens //
+		List<Allergen> allergens = new ArrayList<Allergen>();
+		// we add the allergens to the arraylist of allergens //
+		addElements(allergens);
+		// we check if the arraylist is empty o full //
+		System.out.println(allergens.size());
 		
+		
+		// PRUEBAS SOBRE LA ARRAYLIST PARA VER SI VA BIEN //
+		
+		// we print the allergens arraylist //
+		System.out.println(allergens);
+		// extract the zero position of the arraylist //
+		System.out.println(allergens.get(0));
+		// metodo que me inventé que puede ser util //
+		System.out.println(lookAllergen("Dairy", allergens));
 	}
 
 }
