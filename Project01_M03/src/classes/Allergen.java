4fe181dadbeb6540d31296127287e178d231f7fe
Jordi Felip <jordifelipzayas@gmail.com>
2015-10-01 17:47:44 +0200

    Last commit

--- Project01_M03/src/classes/Allergen.java
+++ Project01_M03/src/classes/Allergen.java
@@ -1,6 +1,27 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Allergen {
-	int code;
+	// the code is the index of the arrayList //
+	private List<String> allergens;
+
+	public Allergen(){
+		allergens = new ArrayList<String>();
+	}
+	// getter //
+	public List<String> getAllergens() {
+		return allergens;
+	}
+
+	// setter //
+	public void setAllergens(List<String> allergens) {
+		this.allergens = allergens;
+	}
+	
+	
 	
 }
