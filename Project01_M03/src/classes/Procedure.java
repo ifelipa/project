1dc1dc4e4675b8608cd6d7863927234186de5647
isma <ismael.felipa@gmail.com>
2015-10-04 13:34:40 +0200

    +Avances procedure

--- Project01_M03/src/classes/Procedure.java
+++ Project01_M03/src/classes/Procedure.java
@@ -3,23 +3,36 @@ package classes;
 public class Procedure {
 	int code;
 	String name;
-	
 
-	public Procedure(int code, String name){
+	public Procedure(int code, String name) {
 		this.code = code;
 		this.name = name;
 	}
+
 	public int getCode() {
 		return code;
 	}
+
 	public void setCode(int code) {
 		this.code = code;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	/*
+	 * ProceedIngredient
+	 * @param codeProcedure is integer, It´s the code of procedure.
+	 * @param codeIngredient is integer, It´s the code of Ingredient.
+	 * @param quantity is integer, It´s  int times, int time, int temperature
+	 * 
+	 */
+	public String ProceedIngredient(int codeProcedure, Ingredient codeIngredient, int quantity, int times, int time, int temperature) {
+		return this.getName()+" "+quantity +" "+ codeIngredient.getName()+" "+ time+ " "+ temperature ;
+	}
 }
