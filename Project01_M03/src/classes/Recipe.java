dfbbddc180def04f90a560ddd2291b26a9b5943e
Jordi Felip <jordifelipzayas@gmail.com>
2015-10-17 00:22:24 +0200

    Relacion Recipe y Allergen. Todo va OK. Falta la clase Ingrediente crearla y meterla en el recipe y hacer todo su control en el main

--- Project01_M03/src/classes/Recipe.java
+++ Project01_M03/src/classes/Recipe.java
@@ -13,100 +13,120 @@ public class Recipe {
 	double proteines;
 	int fat;
 	int salt;
-	ArrayList<Allergen> allergens;
+	Allergen a;
 	
-	/*
-	 * Method which returns the recipe code 
-	 */
+	
+	public Recipe(int code,String name, int quantity,ArrayList<Ingredient> ingredients,double kcal, double carbohidrates, double proteines, int fat, int salt, Allergen a ){
+		this.code = code;
+		this.name = name;
+		this.quantity = quantity;
+		this.ingredients = ingredients;
+		this.kcal = kcal;
+		this.carbohidrates = carbohidrates;
+		this.proteines = proteines;
+		this.fat = fat;
+		this.salt = salt;
+		this.a = a;			
+	}
+
+
 	public int getCode() {
 		return code;
 	}
-	
-	/*
-	 * Method to set the code of our recipe
-	 * 
-	 */
+
+
 	public void setCode(int code) {
 		this.code = code;
 	}
-	
-	/*
-	 * Method which returns the recipe name
-	 */
-	
+
+
 	public String getName() {
 		return name;
 	}
-	
-	/*
-	 *	Method which sets the recipe name 
-	 */
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	/*
-	 * Method which returns the recipe quantity
-	 */
+
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
-	/*
-	 * Method to set the quantity of our recipe
-	 */
+
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-	
+
+
 	public ArrayList<Ingredient> getIngredients() {
 		return ingredients;
 	}
+
+
 	public void setIngredients(ArrayList<Ingredient> ingredients) {
 		this.ingredients = ingredients;
 	}
+
+
 	public double getKcal() {
 		return kcal;
 	}
+
+
 	public void setKcal(double kcal) {
 		this.kcal = kcal;
 	}
+
+
 	public double getCarbohidrates() {
 		return carbohidrates;
 	}
+
+
 	public void setCarbohidrates(double carbohidrates) {
 		this.carbohidrates = carbohidrates;
 	}
+
+
 	public double getProteines() {
 		return proteines;
 	}
+
+
 	public void setProteines(double proteines) {
 		this.proteines = proteines;
 	}
+
+
 	public int getFat() {
 		return fat;
 	}
+
+
 	public void setFat(int fat) {
 		this.fat = fat;
 	}
+
+
 	public int getSalt() {
 		return salt;
 	}
+
+
 	public void setSalt(int salt) {
 		this.salt = salt;
 	}
-	public ArrayList<Allergen> getAllergens() {
-		return allergens;
+
+
+	public Allergen getAllergen() {
+		return a;
 	}
-	public void setAllergens(ArrayList<Allergen> allergens) {
-		this.allergens = allergens;
+
+
+	public void setAllergen(Allergen a) {
+		this.a = a;
 	}
 	
-	/*
-	 * Print recipe
-	 */
-	@Override
-	public String toString(){
-		return this.code + this.name + this.quantity + this.ingredients + this.kcal + this.carbohidrates + this.proteines + this.fat + this.salt + this.allergens;
-	}
 }
--- Project01_M03/src/main/Main.java
+++ Project01_M03/src/main/Main.java
@@ -1,9 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeMap;
-
 // we import all classes added in the package classes //
 import classes.*;
 
@@ -11,12 +7,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// we create one arraylist of allergens //
-//		Allergen listaAler = new Allergen();
-//		System.out.println(listaAler.returnCode("eggs"));
-//		System.out.println(listaAler.returnName(2));
-//		System.out.println(listaAler.returnName(-2));
-//		
+
+
 		//Prueba
 		Allergen a = new Allergen();
 		System.out.println(a.tamany());
@@ -24,7 +16,11 @@ public class Main {
 		System.out.println(a.tamany());
 		System.out.println(a.getElement(13));
 		System.out.println(a.getElement("Lupin"));
-//		Ingredient arroz = new Ingredient(01,"Arroz","kg",10,15,1,3,2,alergicoProducto);
+
+		Recipe b = new Recipe(0, "Arroz con algo", 2, null, 200, 300, 150, 150, 5, a);
+		Allergen prueba = b.getAllergen();
+		System.out.println(prueba.tamany());
+		//Ingredient arroz = new Ingredient(01,"Arroz","kg",10,15,1,3,2,alergicoProducto);
 //		System.out.println(arroz.containAllergen(eggs));	
 //		Procedure pr=new Procedure(1, "Hervir");
 //		System.out.println(pr.ProceedIngredient(01, arroz, 10, 1, 45, 45));
