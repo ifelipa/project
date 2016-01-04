ed92ad315a2d9e7e7f302162b23d0682af0c953e
isma <ismael.felipa@gmail.com>
2016-01-04 15:48:54 +0100

    +avances

--- Project01_M03/src/main/mainPizza.java
+++ Project01_M03/src/main/mainPizza.java
@@ -11,28 +11,24 @@ import db.DbUtil;
 public class mainPizza {
 
 	public static void main(String[] args) {
-		//
-		// insertIngredient(7, "wheat flour", 2, 341.0, 90.6, 9.86, 0.0, 2.0,
-		// "250");
-		// insertIngredient(8, "water", 3, 0, 0, 0, 0, 0, "200");
-		// insertIngredient(9, "yeast", 2, 164.0, 27.80, 11.8, 0, 360.00,
-		// "100");
-		// insertIngredient(10, "olive oil", 3, 900.0, 1.0, 0.0, 0, 0.0, "20");
-		// insertIngredient(11, "shredded cheese", 2, 392.84, 25.39, 0.36, 0,
-		// 675, "150");
-		// insertIngredient(12, "Tomate sauce", 3, 38.1, 2.30, 5.55, 0, 990,
-		// "200");
-		//
-		createRecipe(2, "Pizza", 1, 1835.94, 108.35, 66.35, 0.0, 486.0, "1,14");
-		//
-		// createProcediment(6, "knead until");
-		// createProcediment(7, "let raise");
-		// createProcediment(8, "knead dough again");
-		// createProcediment(9, "lay on oven");
-		// createProcediment(10, "lay on oven");
-		// createProcediment(11, "Bake in oven ");
-		//
-		// createStepXRecipe();
+		
+//		 insertIngredient(7, "wheat flour", 2, 341.0, 90.6, 9.86, 0.0, 2.0, "250");
+//		 insertIngredient(8, "water", 3, 0, 0, 0, 0, 0, "200");
+//		 insertIngredient(9, "yeast", 2, 164.0, 27.80, 11.8, 0, 360.00,"100");
+//		 insertIngredient(10, "olive oil", 3, 900.0, 1.0, 0.0, 0, 0.0, "20");
+//		 insertIngredient(11, "shredded cheese", 2, 392.84, 25.39, 0.36, 0,675, "150");
+//		 insertIngredient(12, "Tomate sauce", 3, 38.1, 2.30, 5.55, 0, 990, "200");
+
+		// createRecipe(2, "Pizza", 1, 1835.94, 108.35, 66.35, 0.0, 486.0, "1,14");
+		
+		 createProcediment(6, "knead until");
+		 createProcediment(7, "let raise");
+		 createProcediment(8, "knead dough again");
+		 createProcediment(9, "lay on oven");
+		 createProcediment(10, "lay on oven");
+		 createProcediment(11, "Bake in oven ");
+		
+		// createStepXRecipe(1,1);
 
 	}
 
@@ -93,7 +89,7 @@ public class mainPizza {
 		Connection conexion = null;
 		PreparedStatement pr = null;
 
-		String sql = "insert into recipe values(?,'?',?,?,?,?,?,?,'?');";
+		String sql = "insert into recipe values(?,?,?,?,?,?,?,?,?)";
 
 		try {
 			conexion = Conectar.getInstance().createConnection();
@@ -109,7 +105,7 @@ public class mainPizza {
 			pr.setDouble(8, salt);
 			pr.setString(9, aler);
 
-			if (pr.execute(sql)) {
+			if (pr.execute()) {
 				System.out.println("Hubo un error al ingresar: " + name);
 			} else {
 				System.out.println("Se agrego correctament: " + name);
